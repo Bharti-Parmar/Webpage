0455d2cedc9fd0b4e7990f26de507febd228d104
Bharti Parmar <38555462+Bharti-Parmar@users.noreply.github.com>
2019-08-30 21:33:36 +0530

    Add files via upload

---
+++ b4.php
@@ -0,0 +1,25 @@
+<?php
+include('header.php');
+?>
+
+<nav class="navbar navbar-expand-md navbar-light">
+
+  <a href="#" class="navbar-brand"><i class="fa fa-cube text-light mr-2">CoCubes</i></a>
+
+       <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse"> <span class="navbar-toggler-icon bg-light"></span>
+      </button>
+
+         <div class="collapse navbar-collapse justify-content-end" id="navbarCollapse">
+	  <div class="navbar-nav ">
+	            <a class="nav-item nav-link text-light active mr-5" href="#">Home</a>
+		<a class="nav-item nav-link text-light mr-5" href="#">About</a>
+                        <a class="nav-item nav-link text-light mr-5" href="#">Services</a>
+		<a class="nav-item nav-link text-light mr-5" href="#">Company</a>
+		<a class="nav-item nav-link text-light mr-5" href="#">Contact</a>
+	 </div>
+	</div>
+   </nav>
+
+<?php
+include('footer.php');
+?>
---
+++ register.php
@@ -0,0 +1,27 @@
+ <?php
+    include('connection.php');
+  ?>
+<!-- data registration php file -->
+  <?php
+
+    if(isset($_POST['submit']))  {
+      $f_name=$_POST['f_name'];
+      $l_name=$_POST['l_name'];
+      $email=$_POST['email'];
+      $contact=$_POST['contact'];
+      $s_address=$_POST['s_address'];
+      $DOB=$_POST['DOB'];
+      $gender=$_POST['gender'];
+      $interest=$_POST['select'];
+      $comment=$_POST['comment'];
+    	$date=date('Y/m/d H:i:s');
+
+    	$query = "INSERT INTO table1(f_name,l_name,email,contact,s_address,DOB,gender,interest,comment,date_time)VALUES('".$f_name."','".$l_name."','".$email."','".$contact."','".$s_address."','".$DOB."','".$gender."','".$interest."','".$comment."','".$date."')";
+    	mysqli_query($conn,$query);
+    	header('location:registration_form.php?save=true');
+    }
+  ?>
+
+  <?php
+     include('footer.php');
+   ?>
